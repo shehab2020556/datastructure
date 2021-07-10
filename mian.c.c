f3f77383c3d232808692b532a0f1dad71a5b4abd
shehab <ziadmohamed126@gmail.com>
2021-07-10 23:13:34 +0200

    linked list

--- mian.c.c
+++ mian.c.c
@@ -280,4 +280,299 @@ void enter_at_the_beginnig_dynamic_array(){
 
  }
 
+//*************************************************************
+//*************************************************************
+//********************linked list functions********************
+//*************************************************************
+//*************************************************************
 
+ void create_linked_list(){
+
+    printf("enter the number of students you want to add \n");
+    scanf("%d",&n);
+    if (n<=0){
+    return;
+    }
+    struct studentL *newNode, *temp;
+    head = (struct studentL *)malloc(sizeof(struct studentL));
+
+    printf("\n enter student name \n");
+    scanf("%s",&std_nameT);
+    printf("\n enter student ID \n");
+    scanf("%d",&std_idT);
+    printf("\n enter student birth date day//month//year: \n");
+    scanf("%d",&dayT);
+    scanf("%d",&monthT);
+    scanf("%d",&yearT);
+    printf("\n enter student last year score:");
+    scanf("%d",&std_scoreT);
+
+    strcpy(head->std_nameL,std_nameT);
+    head->std_idL = std_idT;
+    head->dayL = dayT;
+    head->monthL = monthT;
+    head->yearL = yearT;
+    head->std_scoreL = std_scoreT;
+    head->next = NULL;
+    temp = head;
+
+    for(int i=2; i<=n; i++){
+    newNode = (struct studentL *)malloc(sizeof(struct studentL));
+    printf("\n enter student name \n");
+    scanf("%s",&std_nameT);
+    printf("\n enter student ID \n");
+    scanf("%d",&std_idT);
+    printf("\n enter student birth date day//month//year: \n");
+    scanf("%d",&dayT);
+    scanf("%d",&monthT);
+    scanf("%d",&yearT);
+    printf("\n enter student last year score:");
+    scanf("%d",&std_scoreT);
+
+    strcpy(newNode->std_nameL,std_nameT);
+    newNode->std_idL = std_idT;
+    newNode->dayL = dayT;
+    newNode->monthL = monthT;
+    newNode->yearL = yearT;
+    newNode->std_scoreL = std_scoreT;
+    newNode->next = NULL;
+    temp->next = newNode;
+    temp = temp->next;
+
+    }
+}
+
+void show_students_list_linkedlist(){
+    struct studentL*temp;
+
+    if(n<=0)
+    {
+        printf("the list is empty \n");
+    }
+    else
+    {
+        temp = head;
+        while(temp != NULL)
+        {
+           printf("\n\n student name : %s", temp->std_nameL);
+           printf("\n student ID : %d",temp->std_idL);
+           printf("\n student birth date (day//month//year): %d",temp->dayL);
+           printf("// %d",temp->monthL);
+           printf("// %d",temp->yearL);
+           printf("\n student last year score: %d", temp->std_scoreL);
+           printf("\n*******************************\n");
+           temp = temp->next;
+        }
+    }
+}
+
+
+
+void enter_in_the_mddle_linkedlist(){
+
+    struct studentL *newNode, *temp;
+
+    newNode = (struct studentL*)malloc(sizeof(struct studentL));
+
+    printf("\n enter student name \n");
+    scanf("%s",&std_nameT);
+    printf("\n enter student ID \n");
+    scanf("%d",&std_idT);
+    printf("\n enter student birth date day//month//year: \n");
+    scanf("%d",&dayT);
+    scanf("%d",&monthT);
+    scanf("%d",&yearT);
+    printf("\n enter student last year score:");
+    scanf("%d",&std_scoreT);
+
+
+    strcpy(newNode->std_nameL,std_nameT);
+    newNode->std_idL = std_idT;
+    newNode->dayL = dayT;
+    newNode->monthL = monthT;
+    newNode->yearL = yearT;
+    newNode->std_scoreL = std_scoreT;
+    newNode->next = NULL;
+    temp = head;
+
+    for(int i=2; i<new_std; i++)
+        {
+            temp = temp->next;
+            if(temp == NULL)
+                break;
+        }
+
+        if(temp != NULL)
+        {
+            newNode->next = temp->next;
+            temp->next = newNode;
+        }
+
+
+
+}
+
+
+ void enter_at_the_beginnig_linkedlist(){
+
+   struct studentL *newNode;
+   newNode = (struct studentL*)malloc(sizeof(struct studentL));
+
+    printf("\n enter student name \n");
+    scanf("%s",&std_nameT);
+    printf("\n enter student ID \n");
+    scanf("%d",&std_idT);
+    printf("\n enter student birth date day//month//year: \n");
+    scanf("%d",&dayT);
+    scanf("%d",&monthT);
+    scanf("%d",&yearT);
+    printf("\n enter student last year score:");
+    scanf("%d",&std_scoreT);
+
+    strcpy(newNode->std_nameL,std_nameT);
+    newNode->std_idL = std_idT;
+    newNode->dayL = dayT;
+    newNode->monthL = monthT;
+    newNode->yearL = yearT;
+    newNode->std_scoreL = std_scoreT;
+    newNode->next = head;
+    head = newNode;
+
+ }
+
+ void enter_at_the_end_linkedlist(){
+
+    struct studentL *newNode, *temp;
+
+    newNode = (struct studentL*)malloc(sizeof(struct studentL));
+
+    printf("\n enter student name \n");
+    scanf("%s",&std_nameT);
+    printf("\n enter student ID \n");
+    scanf("%d",&std_idT);
+    printf("\n enter student birth date day//month//year: \n");
+    scanf("%d",&dayT);
+    scanf("%d",&monthT);
+    scanf("%d",&yearT);
+    printf("\n enter student last year score:");
+    scanf("%d",&std_scoreT);
+
+
+    strcpy(newNode->std_nameL,std_nameT);
+    newNode->std_idL = std_idT;
+    newNode->dayL = dayT;
+    newNode->monthL = monthT;
+    newNode->yearL = yearT;
+    newNode->std_scoreL = std_scoreT;
+    newNode->next = NULL;
+    temp = head;
+
+    while(temp != NULL && temp->next != NULL){temp = temp->next;}
+
+    temp->next = newNode;
+ }
+
+ void compare (int dummy){
+       printf("\n*******************************************************\n");
+       int* dump;
+       clock_t begin = clock();
+       dump = (int*)malloc(dummy * sizeof(int));
+       for (int i = 0; i < dummy; ++i) {
+            dump[i] = i + 1;
+        }
+        clock_t end = clock();
+        timespent=(double)(end - begin)*1000/ CLOCKS_PER_SEC;
+        printf("time spent to create dynamic array of %d elements: %1f ms \n",dummy ,timespent);
+
+
+
+        clock_t begin6 = clock();
+        dump =(int*)realloc(dump,(dummy+1)*sizeof(int));
+        for(int i=dummy-1;i>=1;i--){
+        dump[i]=dump[i-1];
+        }
+        dump[0]=1;
+        clock_t end6 = clock();
+        timespent=(double)(end6 - begin6)*1000/ CLOCKS_PER_SEC;
+        printf("time spent to insert at the beginning of dynamic array of %d elements: %1f ms \n",dummy ,timespent);
+        clock_t begin5 = clock();
+        dump =(int*)realloc(dump,(dummy+1)*sizeof(int));
+        for(int i=dummy-1;i>=(dummy/2);i--){
+        dump[i]=dump[i-1];
+        }
+        dump[dummy/2]=1;
+        clock_t end5 = clock();
+        timespent=(double)(end5 - begin5)*1000/ CLOCKS_PER_SEC;
+        printf("time spent to insert at the middle of dynamic array of %d elements: %1f ms \n",dummy ,timespent);
+        clock_t begin7 = clock();
+        dump =(int*)realloc(dump,(dummy+1)*sizeof(int));
+        for(int i=dummy-1;i>=dummy+1;i--){
+        dump[i]=dump[i-1];
+        }
+        dump[dummy]=1;
+        clock_t end7 = clock();
+        timespent=(double)(end7 - begin7)*1000/ CLOCKS_PER_SEC;
+        printf("time spent to insert at the end of dynamic array of %d elements: %1f ms \n",dummy ,timespent);
+
+
+        clock_t begin1 = clock();
+        struct node *headnode,*newnode, *temp;
+        headnode = (struct node *)malloc(sizeof(struct node));
+        headnode->test=0;
+        headnode->next = NULL;
+        temp = headnode;
+        for(int i=1 ; i<dummy; i++){
+            newnode = (struct node *)malloc(sizeof(struct node));
+		    newnode->test = i;
+            newnode->next = NULL;
+            temp->next = newnode;
+            temp = temp->next;
+	    }
+        clock_t end1 = clock();
+        timespent=(double)(end1 - begin1)*1000/ CLOCKS_PER_SEC;
+        printf("\ntime spent to create linked list of %d elements: %1f ms \n",dummy,timespent);
+
+        clock_t begin2= clock();
+        struct node* new_node = (struct node*) malloc(sizeof(struct node));
+        new_node->test= 1;
+        new_node->next= headnode;
+        headnode= new_node;
+        clock_t end2 = clock();
+        timespent=(double)(end2 - begin2)*1000/ CLOCKS_PER_SEC;
+        printf("time spent to insert at the beginning of linked list of %d elements: %1f ms \n",dummy,timespent);
+
+        int z=0;
+        clock_t begin4= clock();
+        struct node* new_node2 = (struct node*) malloc(sizeof(struct node));
+        new_node2->test=1;
+        new_node2->next=NULL;
+        struct node *last1=headnode;
+        while (z<(dummy/2)){
+        last1 = last1->next;
+        z++;
+        }
+        last1->next = new_node2;
+        clock_t end4 = clock();
+        timespent=(double)(end4 - begin4)*1000/ CLOCKS_PER_SEC;
+        printf("time spent to insert at the middle of linked list of %d elements: %1f ms \n",dummy,timespent);
+        clock_t begin3= clock();
+        struct node* new_node3 = (struct node*) malloc(sizeof(struct node));
+        new_node3->test=1;
+        new_node3->next=NULL;
+        struct node *last=headnode;
+        while (last->next != NULL){
+        last = last->next;}
+        last->next = new_node3;
+        clock_t end3 = clock();
+        timespent=(double)(end3 - begin3)*1000/ CLOCKS_PER_SEC;
+        printf("time spent to insert at the end of linked list of %d elements: %1f ms \n",dummy,timespent);
+        printf("\n*******************************************************\n");
+        free(dump);
+        struct node*tmp;
+        while(head!=NULL){
+        tmp=head;
+        head=head->next;
+        free(tmp);
+        }
+
+ }
